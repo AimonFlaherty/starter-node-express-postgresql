ae3e27f3073cb9d5725bc06da78b9e573390597d
aimon flaherty <aimonflaherty@gmail.com>
2023-06-14 15:57:57 -0400

    knex setup and migrations

---
+++ knexfile.js
@@ -0,0 +1,26 @@
+// Update with your config settings.
+const path = require("path");
+require("dotenv").config();
+//const{DATABASE_URL} = process.env;
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  development: {
+    client: "postgresql",
+    connection: "postgres://zzmwzgsr:pknHs1N1hR0pLfISJTQcc3AWy0IugPXl@ruby.db.elephantsql.com/zzmwzgsr",
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+  },
+};
+
+const config = {
+  client: "postgresql",
+  connection:
+    "postgres://myfakedatabase:8t6FiWqSDF8GsR--7mrun245I9TofnWd@fakepostgres.db.elephantsql.com:5432/myfakedatabase",
+};
+
+// Initialize a Knex instance by passing `config` as an argument to the Knex module
+const knex = require("knex")(config);
